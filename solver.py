a24bd6987393149f3462948bb824f724b712d15a
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2026-02-13 13:34:34 -0800

    Uniform Cost seeminhly working

--- solver.py
+++ solver.py
@@ -1,16 +1,55 @@
+import heapq
 
 def main():
     currState = (1, 0, 2, 4, 5, 3, 7, 8, 6)
     goal  = (1, 2, 3, 4, 5, 6, 7, 8, 0)
 
+    print(currState.index(0))
+
     display(currState)
     #print(find_blank(ez_puzzle))
 
-    for i in get_neighbors(currState):
-        display(i)
+    # for i in get_neighbors(currState):
+    #     print("Neighbor:")
+    #     display(i)
     
     print(misplaced_tile_hueristic(currState,goal))
     print(manhattan_distance(currState,goal))
+
+    uniform_cost_search(currState, goal)
+
+
+def a_star_search(start, goal):
+    #we want a priority queue that is (cost + heuristic, state)
+    #initilaizing the priority queue and the visited set
+    #pritioty queue will be (cost, state)
+    frontier = []
+    heapq.heappush(frontier,(0, start))
+
+    visited = set()
+
+def uniform_cost_search(start, goal):
+        #initilaizing the priority queue and the visited set
+        #pritioty queue will be (cost, state)
+        frontier = []
+        #push the initial state with cost 0 so we have something to work off of 
+        heapq.heappush(frontier, (0, start))
+        visited = set()
+
+        while (frontier):
+            currCost, state = heapq.heappop(frontier)
+            if state in visited:
+                continue
+            visited.add(state)
+            if state == goal:
+                print("Found the solution with current cost: ", currCost)
+                return state
+            for i in get_neighbors(state):
+                heapq.heappush(frontier, (currCost + 1, i))
+                print("Adding neighbor with cost: ", currCost + 1)
+                display(i)
+            
+        return None
 
 
 
