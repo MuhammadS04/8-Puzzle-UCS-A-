a74bc20545ec9b50ca969960cc0d16724dcbca68
Muhammad Sabeel <muhammadsabeel4@gmail.com>
2026-02-13 01:10:43 -0800

    Swap Function works

--- solver.py
+++ solver.py
@@ -3,8 +3,10 @@ def main():
     ez_puzzle = (1,2,3,4,5,6,7,8,0)
 
     print(display(ez_puzzle))
-
     print(find_blank(ez_puzzle))
+
+    for i in get_neighbors(ez_puzzle):
+        print(display(i))
 
 
 
@@ -14,7 +16,8 @@ def find_blank(state):
         #get the row and column of the blank tile
         row = state.index(0) // 3
         col = state.index(0) % 3
-        return (row,col)
+        blank_index = state.index(0)
+        return (row,col, blank_index)
 
 
 def display(state):
@@ -23,6 +26,22 @@ def display(state):
     print(state[3:6])
     print(state[6:9])
 
+def get_neighbors(state):
+    neighbors = []
+    row, col, blank_index = find_blank(state)
+
+    if row > 0:
+        new_blank = blank_index - 3
+        new_state = swap(state, blank_index, new_blank)
+        neighbors.append(new_state)
+
+    return neighbors
+
+def swap(state, index1, index2):
+    temp = list(state)
+    temp[index1], temp[index2] = temp[index2], temp[index1]
+
+    return tuple(temp)
 
 if __name__ == "__main__":
     main()
